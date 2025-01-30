8ae30e810a2160232ddd6fbc2c91b723f4c3b966
DanielStivenRivera <danestiv0610@gmail.com>
2025-01-30 14:08:48 -0500

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-non-null-assertion": "error",
+      "no-console": "warn",
+      "prefer-const": "error",
+      "no-magic-numbers": ["warn", { "ignore": [0, 1] }],
+      "consistent-return": "error",
+      "no-duplicate-imports": "error",
+      "@angular-eslint/component-class-suffix": ["error", { "suffixes": ["Component"] }],
+      "@angular-eslint/directive-class-suffix": ["error", { "suffixes": ["Directive"] }],
+      "@angular-eslint/no-input-rename": "error",
+      "@angular-eslint/no-output-rename": "error",
+      "no-return-await": "error",
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+      "indent": ["error", 2],
+      "max-len": ["error", { "code": 100 }],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {
+      "max-len": ["error", { "code": 100 }],
+    },
+  }
+);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatFormFieldModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
